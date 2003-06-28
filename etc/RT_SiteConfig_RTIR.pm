684c0d6b313d447e1d9fe239564aef65da3cbef2
Linda Julien <leira@bestpractical.com>
2003-06-28 15:31:45 -0400

    defaults and business_hours example in config file

--- etc/RT_SiteConfig_RTIR.pm
+++ etc/RT_SiteConfig_RTIR.pm
@@ -2,16 +2,72 @@
 
 Set($rtirname , "RTIR for " . $Organization);
 
+
 # Set the number of days a message awaiting an external response
 # may be inactive before the ticket becomes overdue
 
 Set($overdueafter, 7);
 
+
 # Set the hash of whois servers
-Set($whois, {1 => "localhost", 2=> "whois.fucknsi.com"});
+Set($whois, {1 => "localhost", 2 => "whois.fucknsi.com"});
+
 
 # Set the number of minutes for the SLA
+
 Set($SLA, {'Full service' => 60, 'Reduced service' => 120});
+
+
+# Set the defaults for RTIR custom fields
+# default values are case-sensitive
+
+Set($_RTIR_SLA_default, "Full service");
+Set($_RTIR_HowReported_default, "Email");
+#Set($_RTIR_ReporterType_default, "");
+#Set($_RTIR_IP_default, "");
+#Set($_RTIR_NetMask_default, "");
+#Set($_RTIR_Port_default, "");
+#Set($_RTIR_WhereBlocked_default, "");
+Set($_RTIR_Constituency_default, "EDUNET");
+#Set($_RTIR_Function_default, "");
+#Set($_RTIR_Classification_default, "");
+
+
+# Set the Business Hours for your organization
+# if left unset, defaults are Monday through Friday 09:00 to 18:00
+
+#Set($BusinessHours, 
+#  {
+#    0 => { Name => 'Sunday',
+#           Start => undef,
+#	   End => undef},
+#
+#    1 => { Name => 'Monday',
+#           Start => '09:00',
+#	   End => '18:00'},
+#
+#    2 => { Name => 'Tuesday',
+#           Start => '09:00',
+#	   End => '18:00'},
+#
+#    3 => { Name => 'Wednesday',
+#           Start => '09:00',
+#	   End => '18:00'},
+#
+#    4 => { Name => 'Thursday',
+#           Start => '09:00',
+#	   End => '18:00'},
+#
+#    5 => { Name => 'Friday',
+#           Start => '09:00',
+#	   End => '18:00'},
+#
+#    6 => { Name => 'Saturday',
+#           Start => undef,
+#           End => undef},
+#  }
+#);
+
 
 # This is the string that indicates a reply, and which will be
 # pre-pended to subjects when you reply to tickets.
