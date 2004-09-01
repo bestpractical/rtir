43d1cb8678b55d2a6c43475a6d8a88df22d46330
Linda Julien <leira@bestpractical.com>
2004-09-01 20:11:55 +0000

    Incidents don't have due dates.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rtir/branches/1.1@1393 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- etc/RTIR_Config.pm
+++ etc/RTIR_Config.pm
@@ -96,6 +96,6 @@ __Requestors__,__OwnerName__,__CreatedRelative__,__DueRelative__});
 Set ($RTIRLinkIncidentSearchResultFormat, qq{___RTIR_Radio__,
 '<B><A HREF="$RT::WebPath/Ticket/Display.html?id=__id__">__id__</a></B>/TITLE:#',
 '<B><A HREF="$RT::WebPath/Ticket/Display.html?id=__id__">__Subject__</a></B>/Title:Subject',
-__Requestors__,__OwnerName__,__CreatedRelative__,__DueRelative__});
+__Requestors__,__OwnerName__,__CreatedRelative__});
 
 1;
