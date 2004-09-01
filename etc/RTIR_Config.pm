1a86e025385244ab6f8e20b92296387108f75a7f
Linda Julien <leira@bestpractical.com>
2004-09-01 06:59:59 +0000

    Initial pass of making BulkLink work with new components.
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rtir/branches/1.1@1392 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- etc/RTIR_Config.pm
+++ etc/RTIR_Config.pm
@@ -88,4 +88,14 @@ Set ($RTIRMergeSearchResultFormat, qq{___RTIR_Radio__,
 '<B><A HREF="$RT::WebPath/Ticket/Display.html?id=__id__">__Subject__</a></B>/Title:Subject',
 __Requestors__,__OwnerName__,__CreatedRelative__,__DueRelative__});
 
+Set ($RTIRLinkSearchResultFormat, qq{___RTIR_Check__,
+'<B><A HREF="$RT::WebPath/Ticket/Display.html?id=__id__">__id__</a></B>/TITLE:#',
+'<B><A HREF="$RT::WebPath/Ticket/Display.html?id=__id__">__Subject__</a></B>/Title:Subject',
+__Requestors__,__OwnerName__,__CreatedRelative__,__DueRelative__});
+
+Set ($RTIRLinkIncidentSearchResultFormat, qq{___RTIR_Radio__,
+'<B><A HREF="$RT::WebPath/Ticket/Display.html?id=__id__">__id__</a></B>/TITLE:#',
+'<B><A HREF="$RT::WebPath/Ticket/Display.html?id=__id__">__Subject__</a></B>/Title:Subject',
+__Requestors__,__OwnerName__,__CreatedRelative__,__DueRelative__});
+
 1;
