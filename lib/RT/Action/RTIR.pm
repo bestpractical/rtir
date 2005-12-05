3cd05d8c83c397850e0b0c8951ffde32b4ad6748
Ruslan Zakirov <ruz@bestpractical.com>
2005-12-05 00:06:43 +0000

    new method CreatorCurrentUser
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rtir/branches/1.9-EXPERIMENTAL@4234 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Action/RTIR.pm
+++ lib/RT/Action/RTIR.pm
@@ -6,5 +6,12 @@ package RT::Action::RTIR;
 
 use base 'RT::Action::Generic';
 
+sub CreatorCurentUser {
+    my $self = shift;
+    my $user = new RT::CurrentUser($self->TransactionObj->CurrentUser);
+    $user->Load($self->TransactionObj->Creator);
+    return $user;
+}
+
 
 1;
