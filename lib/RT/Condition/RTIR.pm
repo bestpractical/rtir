a029e6a9daf3ef37dffa00b0ad68dc2cdb7b35fb
Ruslan Zakirov <ruz@bestpractical.com>
2006-03-14 01:06:17 +0000

    add IsStatusChange condition
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rtir/branches/1.9-EXPERIMENTAL@4747 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Condition/RTIR.pm
+++ lib/RT/Condition/RTIR.pm
@@ -7,4 +7,14 @@ package RT::Condition::RTIR;
 
 use base 'RT::Condition::Generic';
 
+sub IsStatusChange {
+    my $self = shift;
+    my $type = $self->TransactionObj->Type;
+    return 1 if $type eq "Status" or
+        ( $type eq "Set" and
+          $self->TransactionObj->Field eq "Status" );
+
+    return 0;
+}
+
 1;
