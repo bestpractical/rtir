877f08ed827e4bf5bcb5db7e9955edb44c61cf1f
Ruslan Zakirov <ruz@bestpractical.com>
2007-01-22 20:58:33 +0000

    split query into two by OR to improve mysql's plan
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rtir/branches/2.1-EXPERIMENTAL@6792 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Action/RTIR_SetConstituency.pm
+++ lib/RT/Action/RTIR_SetConstituency.pm
@@ -37,8 +37,7 @@ sub Commit {
         ." OR Queue = 'Incident Reports'"
         ." OR Queue = 'Investigations'"
         ." OR Queue = 'Blocks'"
-        .")"
-        ." AND ( MemberOf = ". $ticket->Id ." OR HasMember = ". $ticket->Id ." )";
+        .")";
 
     if ( $constituency ) {
         $query .= " AND CF.{_RTIR_Constituency} != '$constituency'";
@@ -46,15 +45,18 @@ sub Commit {
         $query .= " AND CF.{_RTIR_Constituency} IS NOT NULL";
     }
 
-    my $tickets = RT::Tickets->new( $RT::SystemUser );
-    $tickets->FromSQL( $query );
-    while ( my $t = $tickets->Next ) {
-        $RT::Logger->debug( "Ticket #". $t->id ." inherits constituency from ticket #". $ticket->id );
-        my ($res, $msg) = $t->AddCustomFieldValue(
-            Field => '_RTIR_Constituency',
-            Value => $constituency,
-        );
-        $RT::Logger->warning( "Couldn't set CF: $msg" ) unless $res;
+    # do two queries as mysql couldn't optimize things well
+    foreach my $link_type (qw(MemberOf HasMember)) {
+        my $tickets = RT::Tickets->new( $RT::SystemUser );
+        $tickets->FromSQL( $query ." AND $link_type = ". $ticket->Id );
+        while ( my $t = $tickets->Next ) {
+            $RT::Logger->debug( "Ticket #". $t->id ." inherits constituency from ticket #". $ticket->id );
+            my ($res, $msg) = $t->AddCustomFieldValue(
+                Field => '_RTIR_Constituency',
+                Value => $constituency,
+            );
+            $RT::Logger->warning( "Couldn't set CF: $msg" ) unless $res;
+        }
     }
     return 1;
 }
