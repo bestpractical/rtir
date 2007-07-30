55bfa1dfd6da3a23c6e58db392c05e9e0d957106
Turner Hayes <thayes@bestpractical.com>
2007-07-30 17:07:40 +0000

    added check to make sure CustomField in transaction is not State field (causes infinite loop)
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rtir/branches/2.3-EXPERIMENTAL@8314 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Action/RTIR_SetState.pm
+++ lib/RT/Action/RTIR_SetState.pm
@@ -21,18 +21,22 @@ sub Commit {
     my $self = shift;
 
     my $t = $self->TicketObj;
-
     my $cf = RT::CustomField->new( $self->TransactionObj->CurrentUser );
     $cf->LoadByNameAndQueue( Queue => $t->QueueObj->Id, Name => '_RTIR_State' );
     unless ( $cf->Id ) {
         $RT::Logger->warning("Couldn't load '_RTIR_State' CF for queue ". $t->QueueObj->Name );
         return 1;
     }
+ 
+    if ($self->TransactionObj->Type eq 'CustomField' and $self->TransactionObj->Field == $cf->id) {
+	return 1;
+    }
 
     my $state = $self->GetState;
     return 1 unless $state;
 
     my ($res, $msg) = $t->AddCustomFieldValue(Field => $cf->id, Value => $state);
+
     $RT::Logger->warning("Couldn't add custom field value: $msg") unless $res;
     return 1;
 }
