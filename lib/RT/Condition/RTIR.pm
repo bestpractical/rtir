5b3acb82d8ceea8729f84aa42d54549b71a1c7ed
Ruslan Zakirov <ruz@bestpractical.com>
2007-08-21 10:48:12 +0000

    add IsStaff check to ::Condition::RTIR
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rtir/branches/2.3-EXPERIMENTAL@8661 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/RT/Condition/RTIR.pm
+++ lib/RT/Condition/RTIR.pm
@@ -4,8 +4,19 @@ use strict;
 use RT::IR;
 
 package RT::Condition::RTIR;
-
 use base 'RT::Condition::Generic';
+
+=head1 NAME
+
+RT::Condition::RTIR - generic checks
+
+=head1 METHODS
+
+=head2 IsStatusChange
+
+Returns true if it's status change of a ticket.
+
+=cut
 
 sub IsStatusChange {
     my $self = shift;
@@ -15,6 +26,32 @@ sub IsStatusChange {
           $self->TransactionObj->Field eq "Status" );
 
     return 0;
+}
+
+=head2 IsStaff
+
+Returns true if creator of a ticket is a staff member. By staff member
+we mean memeber of a group which name contains 'DutyTeam'.
+
+=cut
+
+sub IsStaff {
+    my $self = shift;
+
+    my $actor_id = $self->TransactionObj->Creator;
+    my $cgm = RT::CachedGroupMembers->new( $RT::SystemUser );
+    $cgm->Limit(FIELD => 'MemberId', VALUE => $actor_id );
+    my $group_alias = $cgm->Join(
+        FIELD1 => 'GroupId', TABLE2 => 'Groups', FIELD2 => 'id'
+    );
+    $cgm->Limit(
+        ALIAS    => $group_alias,
+        FIELD    => 'Name',
+        OPERATOR => 'LIKE',
+        VALUE    => 'DutyTeam',
+    );
+    $cgm->RowsPerPage(1);
+    return $cgm->First? 1 : 0;
 }
 
 1;
