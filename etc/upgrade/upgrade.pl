b67e193a8fbed1b9207900c4635fd3d0dd83004d
Ruslan Zakirov <ruz@bestpractical.com>
2008-03-28 13:49:47 +0000

    fix etc/upgrade/upgrade.pl script it could miss incidents, now we're using temp file to store intermediate results
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/bps-public/rtir/branches/2.3-EXPERIMENTAL@11249 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- etc/upgrade/upgrade.pl
+++ etc/upgrade/upgrade.pl
@@ -16,6 +16,7 @@ CleanEnv();
 
 # Load the config file
 RT::LoadConfig();
+RT->Config->Set( LogToScreen => 'warning' );
 
 #Connect to the database and get RT::SystemUser and RT::Nobody loaded
 RT::Init();
@@ -24,17 +25,26 @@ my $current_user = GetCurrentUser();
 
 # Get the open incidents with no due dates
 my $incidents = RT::Tickets->new( $current_user );
-$incidents->FromSQL(
+my $incidents_query = 
     "Queue = 'Incidents' AND Due <= '1970-01-02'"
-    ." AND ( ". join( ' OR ', map "Status = '$_'", RT::Queue->ActiveStatusArray() ) ." )"
-);
+    ." AND ( ". join( ' OR ', map "Status = '$_'", RT::Queue->ActiveStatusArray() ) ." )";
+$incidents->FromSQL( $incidents_query );
+
+print "\n\nGoing to update due dates of Incidents where it's not set\n";
+
+print "Query for incidents: $incidents_query\n\n";
 
 my $base_query = "( Queue = 'Incident Reports' OR Queue = 'Investigations' OR Queue = 'Blocks' )"
     ." AND ( ". join( ' OR ', map "Status = '$_'", RT::Queue->ActiveStatusArray() ) ." )"
     ." AND Due > '1970-01-02'";
 
+print "Base query for children: $base_query\n\n";
+
 # Get the children for each Incident
 my $children = RT::Tickets->new( $current_user );
+
+require File::Temp;
+my $tmp = new File::Temp; # croak on error
 
 FetchNext( $incidents, 1 );
 while ( my $inc = FetchNext( $incidents ) ) {
@@ -43,9 +53,27 @@ while ( my $inc = FetchNext( $incidents ) ) {
     $children->RowsPerPage(1);
     my $child = $children->First or next;
 
-    # Set the due date of the Incident to the due date of the child
-    $inc->SetDue( $child->DueObj->ISO );
+    print $tmp $inc->id ." ". $child->DueObj->ISO ."\n";
 }
+
+seek $tmp, 0, 0;
+while ( my $str = <$tmp> ) {
+    chomp $str;
+    my ($id, $date) = split /\s/, $str, 2;
+    my $inc = RT::Ticket->new( $RT::SystemUser );
+    $inc->Load( $id );
+    unless ( $inc->id ) {
+        print STDERR "Couldn't load incident #$id\n";
+        next;
+    }
+    my ($status, $msg) = $inc->SetDue( $date );
+    unless ( $status ) {
+        print STDERR "Couldn't set due date of the incident #$id: $msg\n";
+        next;
+    }
+    print "Updated inc #$id, due is $date\n";
+}
+print "done.\n";
 
 use constant PAGE_SIZE => 100;
 sub FetchNext {
