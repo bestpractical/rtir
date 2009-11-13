eb776cbec4fe9f838160a943527655e4a9398701
Ruslan Zakirov <ruz@bestpractical.com>
2009-11-13 17:58:28 +0300

    cover more custom field changes in upgrade script

--- etc/upgrade/2.5.1/update_saved_searches.pl
+++ etc/upgrade/2.5.1/update_saved_searches.pl
@@ -6,14 +6,26 @@ use RT;
 RT->LoadConfig;
 RT->Init;
 
+my %special_rename = (
+    howreported => 'How Reported',
+    resportertype => 'Reporter Type',
+    whereblocked => 'Where Blocked',
+);
+
 my $saved_searches = RT::Attributes->new($RT::SystemUser);
 $saved_searches->Limit( FIELD => 'Name', VALUE => 'SavedSearch' );
 while ( my $s = $saved_searches->Next ) {
     my $content    = $s->Content;
     my $old_query  = $content->{Query};
     my $old_format = $content->{Format};
-    $content->{Query}  =~ s/(?<=CF\.{)_RTIR_//ig;
-    $content->{Format} =~ s/(?<=CF\.{)_RTIR_//ig;
+    my $special = join '|', keys %special_rename;
+    foreach ( $content->{Query}, $content->{Format} ) {
+        s/(?<=CF\.{)_RTIR_//ig;
+        s/(?<=CustomField\.{)_RTIR_//ig;
+        s/(?<=CF\.{)_RTIR_//ig;
+        s/(?<=CustomField\.{)_RTIR_//ig;
+        s/\b($special)\b/$special_rename{$1}/ige;
+    }
 
     if ( $old_query ne $content->{Query} || $old_format ne $content->{Format} )
     {
