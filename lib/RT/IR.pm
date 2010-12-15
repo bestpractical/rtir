82f3c277698ad621e19ebb3aacafeb9e6d68f809
Ruslan Zakirov <ruz@bestpractical.com>
2010-12-15 04:04:08 +0300

    camelcase Lifecycle API usage

--- lib/RT/IR.pm
+++ lib/RT/IR.pm
@@ -207,8 +207,8 @@ sub States {
         my $queue = RT::Queue->new($RT::SystemUser);
         $queue->Load($name);
         if ( $queue->id ) {
-            push @states, $queue->lifecycle->active   if $arg{'Active'};
-            push @states, $queue->lifecycle->inactive if $arg{'Inactive'};
+            push @states, $queue->Lifecycle->Active   if $arg{'Active'};
+            push @states, $queue->Lifecycle->Inactive if $arg{'Inactive'};
         }
         else {
             $RT::Logger->error( "failed to load queue $name" );
