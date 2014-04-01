0658f07c8fc59e8cf4b9a7e100cc6b016b2b6e78
Kevin Falcone <falcone@bestpractical.com>
2014-04-01 17:40:40 -0400

    Document Types, which was right next to the Queues method just added.

--- lib/RT/IR.pm
+++ lib/RT/IR.pm
@@ -108,6 +108,12 @@ sub OurQueue {
     return $TYPE{ lc $queue };
 }
 
+=head2 Types
+
+Returns a list of valid L<TicketType>s
+
+=cut
+
 sub Types {
     my $self = shift;
     return values %TYPE;
