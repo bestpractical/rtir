e36cfe719b42a0f0c649531095d6efb62cfe96b8
Kevin Falcone <falcone@bestpractical.com>
2014-08-13 22:27:27 -0400

    Refactor IP copying to be generic CF copying.
    Adds a bunch of error checking, both on arguments as well as ensuring
    that the custom field you're copying between tickets is available on
    both tickets.  Retains the HasValue checking from the original code, so
    you only attempt to add values that are missing on that target.

--- lib/RT/Action/RTIR_MergeIPs.pm
+++ lib/RT/Action/RTIR_MergeIPs.pm
@@ -74,24 +74,11 @@ sub Commit {
     my $target = $uri_obj->Object;
     return 1 if $target->id eq $txn->ObjectId;
 
-    my $has_values = $target->CustomFieldValues( 'IP' );
-
     my $source = RT::Ticket->new( $self->CurrentUser );
     $source->LoadById( $txn->ObjectId );
-    my $add_values = $source->CustomFieldValues( 'IP' );
-    while ( my $value = $add_values->Next ) {
-        my $ip = $value->Content;
-        next if $has_values->HasEntry( $ip );
 
-        my ($status, $msg) = $target->AddCustomFieldValue(
-            Value => $ip,
-            Field => 'IP',
-        );
-        RT->Logger->error("Couldn't add IP address: $msg")
-            unless $status;
-    }
+    return $self->CopyCustomFields( To => $target, From => $source, CF => 'IP' );
 
-    return 1;
 }
 
 RT::Base->_ImportOverlays;
