ebe8874d570c0ed1e34c86923d0863296c94bbf2
Dustin Graves <dustin@bestpractical.com>
2016-02-27 00:28:52 +0000

    popup select queues for RTIR ticket create
    Fixes: I#31743, I#31750

---
+++ static/js/rtir.js
@@ -0,0 +1,52 @@
+jQuery(function() {
+    var getQueryString = function(element) {
+        var href = jQuery(element).attr('href');
+        if (!href) return '';
+        var questionMarkIndex = href.indexOf('?');
+        if (!questionMarkIndex) return '';
+        return href.substring(questionMarkIndex+1);
+    };
+
+    var showModal = function(lifecycle, lifecycleDesc) {
+        var queryString = getQueryString(this);
+        jQuery.get(
+            RT.Config.WebHomePath + '/RTIR/Helpers/CreateInRTIRQueueModal?Lifecycle=' + lifecycle + '&LifecycleDesc=' + lifecycleDesc + '&' + queryString,
+            function(html) {
+                jQuery("<div class='modal'></div>")
+                    .append(html).appendTo("body")
+                    .bind('modal:close', function(ev,modal) { modal.elm.remove(); })
+                    .modal();
+            }
+        );
+    };
+
+    var showReportsModal = function(e) {
+        e.preventDefault();
+        showModal('incident_reports', 'report');
+    };
+
+    var showInvestigationsModal = function(e) {
+        e.preventDefault();
+        showModal('investigations', 'investigation');
+    };
+
+    var showBlocksModal = function(e) {
+        e.preventDefault();
+        showModal('blocks', 'block');
+    };
+
+    var showIncidentsModal = function(e) {
+        e.preventDefault();
+        showModal('incidents', 'incident');
+    };
+
+    jQuery("#reports-create").click(showReportsModal);
+    jQuery("#investigations-launch").click(showInvestigationsModal);
+    jQuery("#blocks-create").click(showBlocksModal);
+    jQuery("#incidents-create").click(showIncidentsModal);
+    jQuery("#create-incident").click(showIncidentsModal);
+
+    jQuery(".create_child_incident_reports").click(showReportsModal);
+    jQuery(".create_child_investigations").click(showInvestigationsModal);
+    jQuery(".create_child_blocks").click(showBlocksModal);
+});
