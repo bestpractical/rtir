0c7aba6c0e2bac674df4fb25f8432b6b7df8dddd
Dustin Graves <dustin@bestpractical.com>
2016-04-29 22:34:12 +0000

    skip RTIR ticket create modal if there's only one choice
    Like Self Service, if there is only one queue the user can create a
    ticket it, they should be taken directly to the create page with no
    popup.

--- static/js/rtir.js
+++ static/js/rtir.js
@@ -12,6 +12,13 @@ jQuery(function() {
         jQuery.get(
             RT.Config.WebHomePath + '/RTIR/Helpers/CreateInRTIRQueueModal?Lifecycle=' + lifecycle + '&LifecycleDesc=' + lifecycleDesc + '&' + queryString,
             function(html) {
+                // If there's only one queue, just create a ticket in it and skip the modal
+                var queues = jQuery(html).find("select[name='Queue'] > option");
+                if (queues.length === 1) {
+                    window.location.href = RT.Config .WebHomePath + '/RTIR/Create.html?Queue=' + queues.first().val();
+                    return;
+                }
+
                 jQuery("<div class='modal'></div>")
                     .append(html).appendTo("body")
                     .bind('modal:close', function(ev,modal) { modal.elm.remove(); })
